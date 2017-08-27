722d6d12a87ffbfe633e4b606cb82e4e7f23443b
Alexey Denisenko <alexey.denisenko.work@gmail.com>
2017-08-27 13:45:32 +0300

    Got rid of debuggable code

--- app/src/main/java/com/example/olden/cryptoexchange/presentation/CurrenciesListActivity.java
+++ app/src/main/java/com/example/olden/cryptoexchange/presentation/CurrenciesListActivity.java
@@ -1,23 +1,15 @@
 package com.example.olden.cryptoexchange.presentation;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
-import com.example.olden.cryptoexchange.CryptoExchangeApplication;
 import com.example.olden.cryptoexchange.R;
-import com.example.olden.cryptoexchange.data.network.api.CryptoCompareService;
-
-import javax.inject.Inject;
 
 public class CurrenciesListActivity extends AppCompatActivity {
-
-    @Inject
-    CryptoCompareService apiService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currencies_list);
-        CryptoExchangeApplication.get(this).get().inject(this);
     }
 }
